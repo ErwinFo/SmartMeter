0b4e7b8736ac6657b0e518a99b08a8de13cbc5c7
Erwin Foekema <erwinfoekema@Erwins-MacBook-Pro.local>
2016-02-25 08:03:55 +0100

    commit processdata

--- NodeApp/processdata.js
+++ NodeApp/processdata.js
@@ -1,17 +1,42 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'erwin',
-  password : '1234',
+/*
+  baudrate: 115200,
+  dataBits: 8,
+  stopBits: 1,
+  parity: 'none'
+*/
+/*
+var serialport = require("serialport");
+
+serialport.list(function (err, ports) {
+   console.log("thisis the list callback");
+   ports.forEach(function(port) {
+       console.log(port.comName);
+       console.log(port.pnpId);
+       console.log(port.manufacturer);
+   });
+ });
+ */
+
+var SerialPort = require("serialport").SerialPort
+var serialPort = new SerialPort("/dev/ttyUSB0", {
+  baudrate: 115200
+}, false); // this is the openImmediately flag [default is true]
+
+serialPort.on('error', function(err) {
+  console.log(err); // THIS SHOULD WORK!
 });
 
-connection.connect(function(err) {
-  // connected! (unless `err` is set)
-  console.log("connected");
+serialPort.open(function (err) {
+  if (err) {
+     console.log(err);
+     return;
+  }
+  console.log('open');
+  serialPort.on('data', function(data) {
+    console.log('data received: ' + data);
+  });  
+  serialPort.write("ls\n", function(err, results) {
+    console.log('err ' + err);
+    console.log('results ' + results);
+  });  
 });
-
-var post  = {id: 1, title: 'Hello MySQL'};
-var query = connection.query('INSERT INTO posts SET ?', post, function(err, result) {
-  // Neat!
-});
-console.log(query.sql); // INSERT INTO posts SET `id` = 1, `title` = 'Hello MySQL'
