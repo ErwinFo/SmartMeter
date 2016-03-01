1d114823bc8e2259bc2da266ab5b5b6d9e18a235
Erwin Foekema <erwinfoekema@Erwins-MacBook-Pro.local>
2016-03-01 21:31:11 +0100

    changes

--- nodeapp/main.js
+++ nodeapp/main.js
@@ -1,20 +1,15 @@
 var express = require('express');
-
 var http = require('http');
 var path = require('path');
 var mongoose = require('mongoose');
 var fs = require('fs');
 var processData = require('./processdata.js');
-
 var app = express();
 
-processData.openSerialPort();
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-
-// processData
 mongoose.connect('mongodb://localhost/smartmeter');
 mongoose.connection.on('error', function (err) {
     console.log('something has gone wrong' + err);
@@ -23,6 +18,8 @@ mongoose.connection.on('error', function (err) {
 fs.readdirSync(__dirname + '/models').forEach(function(filename) {
   if (~filename.indexOf('.js')) require(__dirname + '/models/' + filename)
 });
+
+processData.openSerialPort(mongoose);
 
 app.get('/measurements', function(req, res) {
   mongoose.model('measurements').find(function(err, measurements) {
@@ -42,11 +39,3 @@ app.get('/users', function(req, res) {
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
-// a GET request = a database READ or (a.k.a SELECT)
-// app.get('/measurements', function(req, res) {
-// 
-// });
-// var database = require(./database.js);
-// database.createDB();
-
--- nodeapp/processdata.js
+++ nodeapp/processdata.js
@@ -1,8 +1,9 @@
 var serialport = require('serialport');
 var SerialPort = serialport.SerialPort; // localize object constructor
+var active = false;
 var message = [];
 
-var sp = new SerialPort('/dev/ttyUSB0', {
+var serialPort = new SerialPort('/dev/ttyUSB0', {
     baudrate: 115200,
     dataBits: 8,
     stopBits: 1,
@@ -10,42 +11,92 @@ var sp = new SerialPort('/dev/ttyUSB0', {
     parser: serialport.parsers.readline('\n')
 }, false); // this is the openImmediately flag [default is true]
 
-function openSerialPort(){
-    sp.on('error', function(err) {
+function openSerialPort(mongoose) {
+    serialPort.on('error', function(err) {
         console.log(err);
     });
+    console.log('Opening serialport...');
 
-    sp.open(function(err) {
-        if(err) {
-            console.log(err);
-            return;
+    setInterval(function() {
+        if(!active) {
+            try {
+                if(!active) {
+                    active = true;
+                    console.log('attemptLogging');
+                    var serialPort = new SerialPort('/dev/ttyUSB0', {
+                        baudrate: 115200,
+                        dataBits: 8,
+                        stopBits: 1,
+                        parity: 'none',
+                        parser: serialport.parsers.readline('\n')
+                    });
+
+                    serialPort.on("data", function(data) {
+                        data = data.replace(/\u0000/g, '');
+                        data = data.replace(/\r/g, '');
+
+                        if(data != '') {
+                            message.push(data);
+                        }
+
+                        if(data.charAt(0) === '!') {
+                            console.log('! found in message');
+
+                            var storeMessage = obtainMeasurement(message);
+                            console.log(storeMessage.length);
+                            
+                            serialPort.close(function (err) {
+                                console.log('port closed', err);
+                                active = false;
+                            });                  
+                        }
+                    });
+                }
+            console.log('timout');
+            } catch(e) {
+                // Error means port is not available for listening.
+                console.log(e);
+            }
         }
-        console.log('open\n');
+    }, 30000);
+    // 60000 minute
+    // 3600000 hour
+}
 
-        sp.on('data', function(data) {
+/**
+ * Expose methods to other files
+ */
+module.exports = {
+    openSerialPort: function(mongoose) {
+        openSerialPort(mongoose);
+    }
+}
 
-            data = data.replace(/\u0000/g, '');
-            data = data.replace(/\r/g, '');
+/*
+ *   Helper functions
+ */
+function convertToTimeStamp(date) {
+    // Dates are formatted in YYMMDDHHMMSS 
+    // Example 160301083129
+    return new Date(2000 + +date.substr(0, 2),
+        date.substr(2, 2) - 1,
+        date.substr(4, 2),
+        date.substr(6, 2),
+        date.substr(8, 2),
+        date.substr(10, 2)).getTime();
+}
 
-            if(data != '') {
-                message.push(data);
-            }
+function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(find, 'g'), replace);
+}
 
-            if(data.charAt(0) === '!') {
-                console.log('Done');
-
-                // replace close with timout of 1 hour
-                sp.close();
-                obtainMeasurement(message);
-            }
-        });
-
-        sp.write('ls', function(err, results) {
-            if(err) {
-                console.log('err ' + err);
-            }
-        });
-    });    
+function startsWith(str, prefix) {
+    if(str.length < prefix.length)
+        return false;
+    for(var i = prefix.length - 1;
+        (i >= 0) && (str[i] === prefix[i]); --i)
+        continue;
+    return i < 0;
 }
 
 function obtainMeasurement(message) {
@@ -74,18 +125,22 @@ function obtainMeasurement(message) {
             // Date
             message[i] = message[i].replace('0-0:1.0.0', '');
             message[i] = replaceAll(message[i], replace, '');
-            
+
             dateElectric = convertToTimeStamp(message[i]);
 
-            convertedMessage.push({'dateElectric' : dateElectric});
-            
+            convertedMessage.push({
+                'dateElectric': dateElectric
+            });
+
         } else if(startsWith(message[i], '0-0:96.1.1')) {
             // Equipment identifier
             message[i] = message[i].replace('0-0:96.1.1', '');
             message[i] = replaceAll(message[i], replace, '');
             deviceElectric = message[i];
 
-            convertedMessage.push({'deviceElectric' : deviceElectric});
+            convertedMessage.push({
+                'deviceElectric': deviceElectric
+            });
 
         } else if(startsWith(message[i], '1-0:1.8.1')) {
             // Meter Reading electricity delivered to client (Tariff 1)
@@ -93,15 +148,19 @@ function obtainMeasurement(message) {
             message[i] = replaceAll(message[i], replace, '');
             meter181kWh = message[i];
 
-            convertedMessage.push({'meter181kWh' : Number(meter181kWh)});
-            
+            convertedMessage.push({
+                'meter181kWh': meter181kWh
+            });
+
         } else if(startsWith(message[i], '1-0:2.8.1')) {
             // Meter Reading electricity delivered by client (Tariff 1)
             message[i] = message[i].replace('1-0:2.8.1', '');
             message[i] = replaceAll(message[i], replace, '');
             meter281kWh = message[i];
-            
-            convertedMessage.push({'meter281kWh' : Number(meter281kWh)});
+
+            convertedMessage.push({
+                'meter281kWh': meter281kWh
+            });
 
         } else if(startsWith(message[i], '1-0:1.8.2')) {
             // Meter Reading electricity delivered to client (Tariff 2)
@@ -109,64 +168,80 @@ function obtainMeasurement(message) {
             message[i] = replaceAll(message[i], replace, '');
             meter182kWh = message[i];
 
-            convertedMessage.push({'meter182kWh' : Number(meter182kWh)});
+            convertedMessage.push({
+                'meter182kWh': meter182kWh
+            });
 
         } else if(startsWith(message[i], '1-0:2.8.2')) {
             // Meter Reading electricity delivered by client (Tariff 2)
             message[i] = message[i].replace('1-0:2.8.2', '');
             message[i] = replaceAll(message[i], replace, '');
             meter282kWh = message[i];
-            
-            convertedMessage.push({'meter282kWh' : Number(meter282kWh)});
+
+            convertedMessage.push({
+                'meter282kWh': meter282kWh
+            });
 
         } else if(startsWith(message[i], '0-0:96.14.0')) {
             // Tariff indicator electricity. The tariff indicator can be used to switch tariff dependent loads e.g boilers.
-            // This is responsibility of the P1 user
+            // This is reserialPortonsibility of the P1 user
             message[i] = message[i].replace('0-0:96.14.0', '');
             message[i] = replaceAll(message[i], replace, '');
             tariff = message[i];
 
-            convertedMessage.push({'tariff' : Number(tariff)});
-            
+            convertedMessage.push({
+                'tariff': Number(tariff)
+            });
+
         } else if(startsWith(message[i], '1-0:1.7.0')) {
             // Actual electricity power delivered (+P) in 1 Watt resolution
             message[i] = message[i].replace('1-0:1.7.0', '');
             message[i] = replaceAll(message[i], replace, '');
             powerDeliveredKw = message[i];
-            
-            convertedMessage.push({'powerDeliveredKw' : Number(powerDeliveredKw)});
+
+            convertedMessage.push({
+                'powerDeliveredKw': powerDeliveredKw
+            });
 
         } else if(startsWith(message[i], '1-0:2.7.0')) {
             // Actual electricity power received (-P) in 1 Watt resolution
             message[i] = message[i].replace('1-0:2.7.0', '');
             message[i] = replaceAll(message[i], replace, '');
             powerReceivedKw = message[i];
-            
-            convertedMessage.push({'powerReceivedKw' : Number(powerReceivedKw)});
+
+            convertedMessage.push({
+                'powerReceivedKw': powerReceivedKw
+            });
 
         } else if(startsWith(message[i], '0-0:96.7.21')) {
             // Number of power failures in any phases
             message[i] = message[i].replace('0-0:96.7.21', '');
             message[i] = replaceAll(message[i], replace, '');
             powerFailures = message[i];
-            
-            convertedMessage.push({'powerFailures' : Number(powerFailures)});
+
+            convertedMessage.push({
+                'powerFailures': Number(powerFailures)
+            });
 
         } else if(startsWith(message[i], '0-0:96.7.9')) {
             // Number of long power failures in any phases
             message[i] = message[i].replace('0-0:96.7.9', '');
             message[i] = replaceAll(message[i], replace, '');
             powerFailuresLong = message[i];
-            
-            convertedMessage.push({'powerFailuresLong' : Number(powerFailuresLong)});
+
+            convertedMessage.push({
+                'powerFailuresLong': Number(powerFailuresLong)
+            });
 
         } else if(startsWith(message[i], '0-1:96.1.0')) {
             // Equipment identifier
             message[i] = message[i].replace('0-1:96.1.0', '');
             message[i] = replaceAll(message[i], replace, '');
             deviceGas = message[i];
-            
-            convertedMessage.push({'deviceGas' : deviceGas});
+
+            convertedMessage.push({
+                'deviceGas': deviceGas
+            });
 
         } else if(startsWith(message[i], '0-1:24.2.1')) {
             // Gas measurement
@@ -179,49 +254,16 @@ function obtainMeasurement(message) {
             message[i] = message[i].replace(dateString, '');
             gasMeasurementm3 = message[i].substring(0, 9);
 
-            convertedMessage.push({'dateGas' : dateGas});
-            convertedMessage.push({'gasMeasurementm3' : Number(gasMeasurementm3)});
+            convertedMessage.push({
+                'dateGas': dateGas
+            });
+            convertedMessage.push({
+                'gasMeasurementm3': gasMeasurementm3
+            });
 
             console.log(convertedMessage);
 
             return convertedMessage;
         }
     }
-}
-
-/**
-* Expose methods to other files
-*/
-module.exports = {
-    openSerialPort: function(){
-        openSerialPort();
-        console.log('Opening serialport...');
-    }
-}
-
-/*
-*   Helper functions
-*/
-function convertToTimeStamp(date){
-    // Dates are formatted in YYMMDDHHMMSS 
-    // Example 160301083129
-    return new Date(2000 + +date.substr(0, 2),
-                            date.substr(2, 2) - 1,
-                            date.substr(4, 2),
-                            date.substr(6, 2),
-                            date.substr(8, 2),
-                            date.substr(10, 2)).getTime();
-}
-
-function replaceAll(str, find, replace) {
-    return str.replace(new RegExp(find, 'g'), replace);
-}
-
-function startsWith(str, prefix) {
-    if(str.length < prefix.length)
-        return false;
-    for(var i = prefix.length - 1;
-        (i >= 0) && (str[i] === prefix[i]); --i)
-        continue;
-    return i < 0;
 }
