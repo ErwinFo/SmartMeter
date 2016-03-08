18b5df3e288fb12105ca11577903a1604fbc30d1
Erwin <erwin.foekema@CLOCKWORK.NL>
2016-03-08 14:21:57 +0100

    added example

--- nodeapp/not_used/examples.js
+++ nodeapp/not_used/examples.js
@@ -41,3 +41,23 @@ var dataTest = { 'dateElectric': 1457262943000
               ,'deviceGas': '4730303233353631323233353431363134'
               ,'dateGas': 1457262000000
               ,'gasMeasurementm3': '03208.999' };
+			  
+			  
+// define our function with the callback argument
+function some_function(arg1, arg2, callback) {
+    // this generates a random number between
+    // arg1 and arg2
+    var my_number = Math.ceil(Math.random() *
+        (arg1 - arg2) + arg2);
+    // then we're done, so we'll call the callback and
+    // pass our result
+    callback(my_number);
+}
+
+// call the function
+some_function(5, 15, function(num) {
+    // this anonymous function will run when the
+    // callback is called
+    console.log("callback called! " + num);
+
+});
