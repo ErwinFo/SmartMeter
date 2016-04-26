c4231496e88a3ab4a073fa620df73a43d2b9dffb
Erwin Foekema <erwinfoekema@ba91-5cb2058fq6.work.local>
2016-04-26 15:47:35 +0200

    fixed datepicker

--- public/app/datepicker.js
+++ public/app/datepicker.js
@@ -9,54 +9,31 @@
 
         var vm = this;
         $scope.beforeRender = function (date, $view, $dates, $leftDate, $upDate, $rightDate) {
-            
-            vm.selectedDay;
-            vm.upDate;
+
+            vm.activeDate = false;
             vm.date;
-
-            if (date === undefined) {
-                vm.upDate = $upDate.display;
-            } else if (date != undefined && date != vm.selectedDay) {
-                vm.selectedDay = date;
-                vm.upDate = $upDate.display;
-                vm.date = vm.selectedDay;
-            } else if ($upDate.display != vm.upDate) {
-                console.log('vm.upDate: ' + vm.upDate);
-                // vm.upDate = $upDate.display;
-                vm.selectedDay = null;
-
+            
+            for (var i = 0; i < $dates.length; i++) {
+                if ($dates[i].active) {
+                    vm.activeDate = true;
+                    vm.date = new Date($dates[i].utcDateValue);
+                    break;
+                } 
+            }
+            if(vm.activeDate){
+                console.log(vm.date);
+            } else {
                 var year = parseInt($upDate.display.substring(0, 4));
                 var month;
                 var dateString;
-
-                var activeDate;
-                for(var i = 0 ; i < $dates.length ; i++){
-                    if($dates[i].active){
-                        console.log($dates[i].active + '' + $dates[i].display);
-                        activeDate = true;
-                    }else{
-                        activeDate = false;
-                    }
-                }
-                if(active){
-                    if (vm.upDate.length === 4) {
+                if ($upDate.display.length === 4) {
                     dateString = year + '-01' + '-01';
-                    // console.log(vm.upDate + ':' + dateString);
-                } else if (vm.upDate.length === 8) {
+                } else if ($upDate.display.length === 8) {
                     month = $upDate.display.substring(5, 8);
                     dateString = year + month + '-01';
-                    // console.log(dateString);
                 }
-                } else {
-                    
-                }
-                
-
                 vm.date = new Date(dateString);
-
-
             }
-
         }
     }
 })();
